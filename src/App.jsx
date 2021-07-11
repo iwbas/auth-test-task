09cd77d498464922104d51076da3f3ff1687598c
iwbas <brexceed@gmail.com>
2021-07-11 12:31:33 +0400

    mock routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,50 @@
-import "./App.css";
-
-import React, { Component } from "react";
-import Header from "./components/Header";
-import Alert from "react-bootstrap/Alert";
+import './App.css';
+import Navigation from './components/Navigation';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Alert from 'react-bootstrap/Alert';
+import Login from './components/Login';
 
 function App() {
+
+  return <Login/>
+
   return (
-    <>
-      <Header />
-      <Alert dismissible variant="danger">
-        <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
-        <p>Change this and that and try again.</p>
-      </Alert>
-    </>
+    <Router>
+      <header>
+        <Navigation />
+      </header>
+
+      <main className="Main">
+        <Switch>
+          <Route path="/about">
+            <Alert dismissible variant="danger">
+              <Alert.Heading>Oh snap! You got an error!</Alert.Heading>
+              <p>Change this and that and try again.</p>
+            </Alert>
+            <About />
+          </Route>
+          <Route path="/users">
+            <Users />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </main>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from "./App";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
